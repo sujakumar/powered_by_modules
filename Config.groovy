3f0cf7ceae843914edade2cc522bad4a93f8625e
chriscollingwood <ccollingwood@spireon.com>
2019-06-03 14:23:27 -0700

    Add Config.groovy skeleton example. Removed old Extract.groovy

---
+++ Config.groovy
@@ -0,0 +1,50 @@
+proconCoreDb {
+    username    = '$extract_username'
+    password    = '''$some_pwd'''
+    url         = 'jdbc:mysql://amesnapshot01.i.spireon.com/proconCore_production?zeroDateTimeBehavior=convertToNull&connectTimeout=30000&socketTimeout=60000&autoReconnect=true&allowMultiQueries=true'
+}
+
+automotiveServicesDb {
+    username    = '$extract_username'
+    password    = '''$some_pwd'''
+    url         = 'jdbc:mysql://amesnapshot01.i.spireon.com/automotiveServices_production?zeroDateTimeBehavior=convertToNull&connectTimeout=30000&socketTimeout=60000&autoReconnect=true&allowMultiQueries=true'
+}
+
+cdkAdapterDb {
+    username    = '$extract_username'
+    password    = '''$some_pwd'''
+    url         = 'jdbc:mysql://amesnapshot01.i.spireon.com/cdkAdapter_production?zeroDateTimeBehavior=convertToNull&connectTimeout=30000&socketTimeout=60000&autoReconnect=true&allowMultiQueries=true'
+}
+
+fcmsDb {
+    username	= '$extract_username'
+    password	= '''$some_pwd'''
+    url		= 'jdbc:mysql://ame1-rds-fcms-cluster.cluster-ro-coiumbcrhgrg.us-east-1.rds.amazonaws.com/fcms?zeroDateTimeBehavior=convertToNull&connectTimeout=30000&socketTimeout=60000&autoReconnect=true&allowMultiQueries=true'
+}
+
+dmsIntegrationDb {
+    username    = '$extract_username'
+    password    = '''$some_pwd'''
+    url         = 'jdbc:mysql://amesnapshot01.i.spireon.com/dmsIntegration_production?zeroDateTimeBehavior=convertToNull&connectTimeout=30000&socketTimeout=60000&autoReconnect=true&allowMultiQueries=true'
+}
+
+snowflakeDb {
+    username    = "$sf_username"
+    password    = '''$some_sf_pwd'''
+    warehouse   = "LOAD_WH"
+    db          = "$somedb"
+    schema      = "NSPIRE"
+    url         = "jdbc:snowflake://spireon.snowflakecomputing.com:443/?ssl=on&account=spireon"
+    stage       = "S3_LAKESPIREON_PROD"
+}
+
+
+// where to store the extracted db files
+target {
+    aws {
+        s3 {
+            bucket = 'lakespireon'
+            prefix = 'prod/nspire'
+        }
+    }
+}
