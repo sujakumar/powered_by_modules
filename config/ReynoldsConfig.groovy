13f1573bae8195506ca54520afeef2aa4ec5de39
chriscollingwood <ccollingwood@spireon.com>
2019-07-17 17:08:54 -0700

    Adding Reynolds

---
+++ config/ReynoldsConfig.groovy
@@ -0,0 +1,23 @@
+dbs {
+
+    reynolds {
+
+        excludedColumns = ['password', 'pwd', 'foo']
+
+        tables {
+            dealership
+            reynolds_deal
+            reynolds_message
+            sale_aftermarkets{
+                customId = "(CONCAT(reynolds_deal_id, "||", code))"
+            }
+            store
+        }
+        postSql = [
+                "CALL REYNOLDS.POST_PROCESS();"
+        ]
+
+    }
+
+}
+
