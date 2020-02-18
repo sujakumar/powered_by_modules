6b138dba2afe652cb8fdf42b102ed7b566dd3e8e
chriscollingwood <ccollingwood@spireon.com>
2020-02-18 09:17:17 -0800

    Upping memory for shopbert extract.

--- extractShopbert.sh
+++ extractShopbert.sh
@@ -2,6 +2,9 @@
 
 sdir="$(dirname $(readlink -f $0))"
 
+export JAVA_OPTS=" -Xms4G -Xmx4G"
+export GROOVY_OPTS=" -Xms4G -Xmx4G"
+
 echo "starting..."
 nohup $sdir/extract.sh -l -m 100000 -s 3600 -d 3 -e Shopbert &
 echo "kicked off."
